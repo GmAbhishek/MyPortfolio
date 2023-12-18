7be8d1ac5d4118b6160b4249f1a196131543ac52
Gm_Abhishek <gmabhishek2004@gmail.com>
2023-12-19 02:21:10 +0530

    Added gradient flow

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,19 @@ module.exports = {
         accent2: "#8E8E8E",
         gray: "#F1F1F1",
       },
+      keyframes: {
+        animatedgradient: {
+          '0%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+          '100%': { backgroundPosition: '0% 50%' },
+        },
+      },
+      backgroundSize: {
+        '300%': '300%',
+      },
+      animation: {
+        gradient: 'animatedgradient 6s ease infinite alternate',
+      },
     },
     fontFamily: {
       montserrat: ["var(--font-montserrat)", ...fontFamily.sans],
